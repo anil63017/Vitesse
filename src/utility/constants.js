9542e2bdafafe53edf51235b8cc9563711c4ebdb
Anil Kumar <anil@vitesse-group.com>
2024-01-09 11:58:45 -0600

    constants again

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,26 +1,26 @@
 const AUTH = {
-    LOGIN: 'https://vitesse-backend.onrender.com/v1/auth/login',
-    REGISTER: 'https://vitesse-backend.onrender.com/v1/auth/register',
-    VALIDATE: 'https://vitesse-backend.onrender.com/v1/auth/validate',
+    LOGIN: 'https://vitesse-backend.onrender.com:10000/v1/auth/login',
+    REGISTER: 'https://vitesse-backend.onrender.com:10000/v1/auth/register',
+    VALIDATE: 'https://vitesse-backend.onrender.com:10000/v1/auth/validate',
 }
 
 const EMPLOYEE = {
-    LIST: 'https://vitesse-backend.onrender.com/v1/employee/list',
-    CREATE: 'https://vitesse-backend.onrender.com/v1/employee/create',
-    BULK: 'https://vitesse-backend.onrender.com/v1/employee/bulk',
-    UPDATE: 'https://vitesse-backend.onrender.com/v1/employee/',
+    LIST: 'https://vitesse-backend.onrender.com:10000/v1/employee/list',
+    CREATE: 'https://vitesse-backend.onrender.com:10000/v1/employee/create',
+    BULK: 'https://vitesse-backend.onrender.com:10000/v1/employee/bulk',
+    UPDATE: 'https://vitesse-backend.onrender.com:10000/v1/employee/',
 }
 
 const VENDOR = {
-    LIST: 'https://vitesse-backend.onrender.com/v1/vendor/list',
-    CREATE: 'https://vitesse-backend.onrender.com/v1/vendor/create',
-    BULK: 'https://vitesse-backend.onrender.com/v1/vendor/bulk',
-    UPDATE: 'https://vitesse-backend.onrender.com/v1/vendor/',
+    LIST: 'https://vitesse-backend.onrender.com:10000/v1/vendor/list',
+    CREATE: 'https://vitesse-backend.onrender.com:10000/v1/vendor/create',
+    BULK: 'https://vitesse-backend.onrender.com:10000/v1/vendor/bulk',
+    UPDATE: 'https://vitesse-backend.onrender.com:10000/v1/vendor/',
 }
 
 const IMMIGRATION = {
-    LIST: 'https://vitesse-backend.onrender.com/v1/employee/immigration/list',
-    BULK: 'https://vitesse-backend.onrender.com/v1/employee/immigration/bulk',
+    LIST: 'https://vitesse-backend.onrender.com:10000/v1/employee/immigration/list',
+    BULK: 'https://vitesse-backend.onrender.com:10000/v1/employee/immigration/bulk',
 
 }
 const ROLES = {
